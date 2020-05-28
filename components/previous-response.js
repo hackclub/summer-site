11af024cb27e9f1f351b46adbe9d3af77c9aaf11
Max Wofford <max@maxwofford.com>
2020-05-28 11:48:16 -0400

    Remove lines used in debugging

--- components/previous-response.js
+++ components/previous-response.js
@@ -3,31 +3,6 @@ import { Text } from 'theme-ui'
 import * as timeago from 'timeago.js'
 
 const PreviousResponse = ({ reason, timeSince, status }) => {
-  // const [status, setStatus] = useState('loading')
-  // const [reason, setReason] = useState('')
-  // const [timeSince, setTimeSince] = useState('')
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     let options = {
-  //       maxRecords: 1,
-  //       sort: [{ field: 'Created at', direction: 'desc' }],
-  //       filterByFormula: "{Approved for display} = 1"
-  //     }
-  //     let endpointURL = `https://api2.hackclub.com/v0.1/Pre-register/Applications?select=${JSON.stringify(options)}`
-  //     try {
-  //       let results = await fetch(endpointURL, { mode: 'cors' }).then(r => r.json())
-  //       let reason = results[0].fields
-  //       setReason(reason['What do you want to learn?'])
-  //       setTimeSince(timeago.format(reason['Created at']))
-  //       setStatus('success')
-  //     } catch (e) {
-  //       setStatus('error')
-  //     }
-  //   }
-  //   fetchData()
-  // }, [])
-
   if (status == 'success') {
     return (
       <>
@@ -54,7 +29,6 @@ export const getStaticProps = async () => {
   }
   let endpointURL = `https://api2.hackclub.com/v0.1/Pre-register/Applications?select=${JSON.stringify(options)}`
 
-  props.cake = true
   try {
     let results = await fetch(endpointURL, { mode: 'cors' }).then(r => r.json())
     let reason = results[0].fields
