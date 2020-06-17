7c70ed0f3f22704a80be5df3739de1022d087b37
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-06-17 11:02:09 +0800

    maybeeee this will work?

--- components/scrapbook.js
+++ components/scrapbook.js
@@ -1,92 +1,171 @@
-import { Card, Label, Input, Textarea, Text, Grid } from 'theme-ui'
-import useForm from '../lib/use-form'
-import { timeSince } from '../lib/dates'
-import Submit from './submit'
+import {
+  Box,
+  IconButton,
+  Container,
+  Card,
+  Image,
+  Text,
+  Flex,
+  Heading,
+  Button,
+  Grid,
+  Avatar,
+  Badge,
+  Link as A
+} from 'theme-ui'
 
-const full = { gridColumn: [null, 'span 2'] }
+const Scrapbook = ({ image }) => {
 
-const SignupForm = ({ reason, time }) => {
-  const { status, formProps, useField } = useForm('/api/prereg')
   return (
-    <Card
-      sx={{
-        bg: 'smoke',
-        maxWidth: 'narrow',
-        mx: 'auto'
-      }}
+    <Box
+      as="section"
+      sx={{ bg: 'white', color: 'black', position: 'relative', pb: [4, 5] }}
     >
-      <Grid columns={[null, 2]} gap={3} as="form" {...formProps}>
-        <Label sx={full}>
-          First & last name
-          <Input {...useField('Name')} placeholder="Fiona Hackworth" required />
-        </Label>
-        <Label>
-          Email address
-          <Input
-            {...useField('Email')}
-            placeholder="fiona@hackclub.com"
-            required
-          />
-        </Label>
-        <Label>
-          Age
-          <Input {...useField('Age')} placeholder={16} required />
-        </Label>
-        <Label>
-          City, State
-          <Input
-            {...useField('City & State')}
-            placeholder="Seattle, WA"
-            required
-          />
-        </Label>
-        <Label>
-          Country
-          <Input {...useField('Country')} placeholder="USA" required />
-        </Label>
-        <Label sx={full}>
-          What is something you want to learn this summer?
-          <Textarea
-            {...useField('What do you want to learn?')}
-            placeholder="Write a sentence or two."
-            required
-          />
-          {reason && (
-            <>
-              <Text
-                variant="caption"
-                sx={{ textTransform: 'uppercase', fontSize: 1, mt: 3, mb: 1 }}
-              >
-                {timeSince(time)} someone submitted:
-              </Text>
-              <Text
-                variant="caption"
-                color="slate"
-                sx={{
-                  fontSize: 2,
-                  lineHeight: 'subheading',
-                  borderLeft: '3px solid',
-                  borderColor: 'cyan',
-                  pl: 3
-                }}
-              >
-                {reason}
-              </Text>
-            </>
-          )}
-        </Label>
-        <Submit
-          status={status}
-          labels={{
-            default: 'Submit Pre-Registration',
-            error: 'Something went wrong',
-            success: 'Submitted!'
+      <Container>
+        <Grid
+          columns={[2, 15]}
+          gap={3}
+          sx={{
+            py: [3, 4],
+            h3: { my: 0 },
+            '> div': {
+              px: [2, 3],
+              py: [4, 4],
+              display: 'flex',
+              flexDirection: 'column',
+              gridColumn: ['span 1', 'span 3'],
+              position: 'relative'
+            },
+            h3: {
+              variant: 'text.headline',
+              color: 'white',
+              lineHeight: 'title',
+              my: 0
+            }
           }}
-          sx={full}
-        />
-      </Grid>
-    </Card>
+        >
+          <Box
+            as="aside"
+            sx={{
+              gridRow: [null, 'span 2'],
+              gridColumn: ['span 2', 'span 3']
+            }}
+          >
+            <Heading
+              as="h2"
+              variant="headline"
+              sx={{
+                mt: 0,
+                color: 'red',
+                letterSpacing: 'headline'
+              }}
+            >
+              Summer Scrapbook
+            </Heading>
+            <Heading
+              as="h4"
+              variant="subheadline"
+              sx={{
+                mt: 0,
+                letterSpacing: 'headline'
+              }}
+            >
+              Hack Clubbers are sharing everything they make in their personal scrapbook.
+            </Heading>
+            <Button
+              as="a"
+              variant="cta"
+              href="https://scrapbook.hackclub.com"
+              target="_blank"
+              mt={4}
+              mb={[null, 4, 5]}
+              sx={{ backgroundImage: t => t.util.gradient('red', 'orange') }}
+            >
+              Explore
+            </Button>
+          </Box>
+          <Card
+            as="aside"
+            sx={{
+              gridColumn: ['span 2', 'span 6'],
+              bg: 'blue',
+              backgroundImage: theme => `radial-gradient(
+    ellipse farthest-corner at top left, ${theme.colors.cyan}, ${theme.colors.blue})`,
+              p: { color: 'smoke', fontSize: 2, mt: 1, lineHeight: 'caption' }
+            }}
+          >
+            <Heading as="h3" variant="headline">
+              #ship
+            </Heading>
+            <Text as="p">Share your latest projects & get feedback</Text>
+          </Card>
+          <Card
+            as="aside"
+            sx={{
+              gridColumn: ['span 2', 'span 6'],
+              bg: 'dark',
+              backgroundImage:
+                'url(https://cdn.glitch.com/a7605379-7582-4aac-8f44-45bbdfca0cfa%2Fnight.jpg?v=1587692925213)',
+              backgroundPosition: 'top center',
+              backgroundSize: 'cover',
+              p: { color: 'smoke', fontSize: 2, mt: 1, lineHeight: 'caption' }
+            }}
+          >
+            <Heading as="h3" variant="headline">
+              #hack-night
+            </Heading>
+            <Text as="p">Biweekly mini-hackathon & video call hangout</Text>
+          </Card>
+          <Card bg="cyan">
+            <h3>#lounge</h3>
+          </Card>
+          <Card bg="orange">
+            <h3>#support</h3>
+          </Card>
+          <Card bg="blue">
+            <h3>#code</h3>
+          </Card>
+          <Card
+            bg="red"
+            sx={{
+              backgroundImage: ({ colors }) =>
+                `linear-gradient(${colors.red} 0%, ${colors.red} 16.6666%, ${colors.orange} 16.6666%, ${colors.orange} 33.333%, ${colors.yellow} 33.333%, ${colors.yellow} 50%, ${colors.green} 50%, ${colors.green} 66.6666%, ${colors.blue} 66.6666%, ${colors.blue} 83.3333%, #8067C3 83.3333%, #8067C3 100%)`
+            }}
+          >
+            <h3>#lgbtq</h3>
+          </Card>
+          <Card bg="dark">
+            <h3>#gamedev</h3>
+          </Card>
+          <Card bg="red">
+            <h3>#design</h3>
+          </Card>
+          <Card bg="green">
+            <h3>
+              #photo-
+              <br />
+              graphy
+            </h3>
+          </Card>
+          <Card
+            bg="yellow"
+            sx={{
+              backgroundImage:
+                'url(https://cdn.glitch.com/a7605379-7582-4aac-8f44-45bbdfca0cfa%2FImage%20from%20iOS%202.jpg?v=1587695033603)',
+              backgroundSize: '100%',
+              backgroundPosition: 'center',
+              textShadow: 'text'
+            }}
+          >
+            <h3>#dogs</h3>
+          </Card>
+          <Card bg="green">
+            <h3>#music</h3>
+          </Card>
+        </Grid>
+      </Container>
+    </Box>
   )
 }
 
-export default SignupForm
+export default Scrapbook
