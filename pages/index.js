03614ed327c1675af1c8ba4bdab6ae12792bde06
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-06-17 09:18:03 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -540,6 +540,135 @@ export default props => (
       </Container>
     </Box>
     <Box
+      as="section"
+      sx={{ bg: 'white', color: 'black', position: 'relative', pb: [4, 5] }}
+    >
+      <Container>
+        <Grid
+          columns={[2, 15]}
+          gap={3}
+          sx={{
+            py: [3, 4],
+            h3: { my: 0 },
+            '> div': {
+              px: [2, 3],
+              py: [4, 4],
+              display: 'flex',
+              flexDirection: 'column',
+              gridColumn: ['span 1', 'span 3'],
+              position: 'relative'
+            },
+            h3: {
+              variant: 'text.headline',
+              color: 'white',
+              lineHeight: 'title',
+              my: 0
+            }
+          }}
+        >
+          <Box
+            as="aside"
+            sx={{
+              gridRow: [null, 'span 2'],
+              gridColumn: ['span 2', 'span 3']
+            }}
+          >
+            <Heading
+              as="h2"
+              variant="subheadline"
+              sx={{
+                mt: 0,
+                color: 'red',
+                textTransform: 'uppercase',
+                letterSpacing: 'headline'
+              }}
+            >
+              Live from our&nbsp;Slack
+            </Heading>
+            <SlackEvents />
+          </Box>
+          <Card
+            as="aside"
+            sx={{
+              gridColumn: ['span 2', 'span 6'],
+              bg: 'blue',
+              backgroundImage: theme => `radial-gradient(
+    ellipse farthest-corner at top left, ${theme.colors.cyan}, ${theme.colors.blue})`,
+              p: { color: 'smoke', fontSize: 2, mt: 1, lineHeight: 'caption' }
+            }}
+          >
+            <Heading as="h3" variant="headline">
+              #ship
+            </Heading>
+            <Text as="p">Share your latest projects & get feedback</Text>
+          </Card>
+          <Card
+            as="aside"
+            sx={{
+              gridColumn: ['span 2', 'span 6'],
+              bg: 'dark',
+              backgroundImage:
+                'url(https://cdn.glitch.com/a7605379-7582-4aac-8f44-45bbdfca0cfa%2Fnight.jpg?v=1587692925213)',
+              backgroundPosition: 'top center',
+              backgroundSize: 'cover',
+              p: { color: 'smoke', fontSize: 2, mt: 1, lineHeight: 'caption' }
+            }}
+          >
+            <Heading as="h3" variant="headline">
+              #hack-night
+            </Heading>
+            <Text as="p">Biweekly mini-hackathon & video call hangout</Text>
+          </Card>
+          <Card bg="cyan">
+            <h3>#lounge</h3>
+          </Card>
+          <Card bg="orange">
+            <h3>#support</h3>
+          </Card>
+          <Card bg="blue">
+            <h3>#code</h3>
+          </Card>
+          <Card
+            bg="red"
+            sx={{
+              backgroundImage: ({ colors }) =>
+                `linear-gradient(${colors.red} 0%, ${colors.red} 16.6666%, ${colors.orange} 16.6666%, ${colors.orange} 33.333%, ${colors.yellow} 33.333%, ${colors.yellow} 50%, ${colors.green} 50%, ${colors.green} 66.6666%, ${colors.blue} 66.6666%, ${colors.blue} 83.3333%, #8067C3 83.3333%, #8067C3 100%)`
+            }}
+          >
+            <h3>#lgbtq</h3>
+          </Card>
+          <Card bg="dark">
+            <h3>#gamedev</h3>
+          </Card>
+          <Card bg="red">
+            <h3>#design</h3>
+          </Card>
+          <Card bg="green">
+            <h3>
+              #photo-
+              <br />
+              graphy
+            </h3>
+          </Card>
+          <Card
+            bg="yellow"
+            sx={{
+              backgroundImage:
+                'url(https://cdn.glitch.com/a7605379-7582-4aac-8f44-45bbdfca0cfa%2FImage%20from%20iOS%202.jpg?v=1587695033603)',
+              backgroundSize: '100%',
+              backgroundPosition: 'center',
+              textShadow: 'text'
+            }}
+          >
+            <h3>#dogs</h3>
+          </Card>
+          <Card bg="green">
+            <h3>#music</h3>
+          </Card>
+        </Grid>
+      </Container>
+    </Box>
+    <Box
       as="footer"
       sx={{ bg: 'smoke', color: 'black', py: [4, 5], a: { color: 'orange' } }}
     >
@@ -571,6 +700,7 @@ export default props => (
       </Container>
     </Box>
   </>
+  
 )
 
 export const getStaticProps = async () => {
