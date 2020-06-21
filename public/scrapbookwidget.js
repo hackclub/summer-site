90a74a28cbd99f09d05b118776dd4a4e0c047cf8
Lachlan Campbell <lachlan@hackclub.com>
2020-06-21 11:23:11 -0400

    Improve scrapbook widget design (#26)

--- public/scrapbookwidget.js
+++ public/scrapbookwidget.js
@@ -1,20 +1,12 @@
+// Edit here: https://codepen.io/lachlanjc/pen/MWKpvZP?editors=0011
 function displayScrapbookUsername(username) {
-  var proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  var targetUrl = "https://scrapbook.hackclub.com/api/users/" + username;
-  fetch(proxyUrl + targetUrl)
-    .then(resp => resp.json())
-    .then(function(data) {
-      console.log(data["profile"]["streakCount"]);
-      var d1 = document.getElementsByTagName("BODY")[0];
-      d1.insertAdjacentHTML(
-        "beforeend",
-        '<div class="scrapbookButtonWrapper" style="position:fixed;bottom:20px;right:20px;"> <button class="scrapbookButton" onclick ="window.open(\'https://scrapbook.hackclub.com/'+username+'\')" style="border-radius: 50%; height: 60px; width: 60px; font-size: 30px; background-color: #1f2d3d;">🔥</button><style>.scrapbookButton:hover:after {content: "' +
-          data["profile"]["streakCount"] +
-          '";text-indent: 0; display: block; cursor: pointer; line-height: initial; margin-bottom: 0.67em;} .scrapbookButton:hover { padding-top: 10px; text-indent: -9999px; line-height: 0; color: #ff8c37; font-weight: bold;} .scrapbookButton:focus{ outline: none; box-shadow: 0 0 10px rgba(51, 142, 218, 1), 0 0 10px rgba(51, 142, 218,1);}</style></div>'
-      );
-    })
-
-    .catch(function(error) {
-      console.log(error);
-    });
+  fetch('https://scrapbook.hackclub.com/api/users/' + username).then(function (res) {
+    return res.json();
+  }).then(function (data) {
+    return data.profile.streakCount;
+  }).then(function (streak) {
+    document.body.insertAdjacentHTML('beforeend', "<a class=\"scrapbookButton\" href=\"https://scrapbook.hackclub.com/".concat(username, "\">\uD83D\uDD25 ").concat(streak, "</a>\n<style>\n.scrapbookButton {\nposition: fixed; bottom: 24px; right: 24px; border-radius: 50%; height: 64px; width: 64px; font-size: 20px; font-weight: 600; color: #fff; font-family: system-ui, Roboto, Helvetica, sans-serif; line-height: 64px; background-color: #1f2d3d; text-align: center; text-decoration: none; transition: .125s background-color ease-in-out;\n}\n.scrapbookButton:hover, .scrapbookButton:focus { background-color: #f7ff00; color: #1d201d; }\n.scrapbookButton:focus { outline: none; box-shadow: 0 0 10px rgba(51, 142, 218, 1), 0 0 10px rgba(51, 142, 218,1); }\n</style>"));
+  }).catch(function (error) {
+    console.error(error);
+  });
 }
