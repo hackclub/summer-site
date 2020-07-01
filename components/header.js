6f6aff286ad6f4225576491776a5c84217d94626
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-07-01 17:41:42 +0800

    Update header.js

--- components/header.js
+++ components/header.js
@@ -39,7 +39,7 @@ const Sheet = () => (
       variant="title"
       sx={{ color: 'black', span: { color: 'pink', display: 'block' } }}
     >
-      Today begins the{' '}
+      TWelcome now to the{' '}
       <Text as="span" sx={t => t.util.gradientText('orange', 'pink')}>
         Summer of Making.
       </Text>
