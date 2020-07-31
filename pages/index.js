08c4f0e5c7cb3f82fc758dd87a108369b61a01b3
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-07-31 19:45:12 +0800

    Add Stuff so I can show Rishi

--- pages/index.js
+++ pages/index.js
@@ -54,6 +54,33 @@ export default () => (
       </Text>
     </Container>
     <Scrapbook />
+    <Container
+      variant="copy"
+      sx={{
+        my: 3,
+        py: 3,
+        border: '2px dashed',
+        borderColor: 'sunken',
+        borderRadius: 'default',
+        mt: [3, 4],
+        mb: [3, 4]
+      }}
+    >
+      <Text
+        sx={{
+          fontSize: '24px'
+        }}
+      >
+        This will be changed to something :) 53 years ago, 100,000 people gathered in San
+        Francisco for the Summer of Love. Welcome to the Summer of Making. It’s
+        not an event or program. It’s a theme for the summer, a challenge to
+        every teenager reading this: What will you make? To suppport you, we’re
+        distributing $50,000 in free electronics (thanks GitHub!) & building
+        Snapchat streaks for coding—but really Summer of Making is about you. If
+        this will be the best summer ever, it’ll be because of what you choose
+        to hack on & learn from.
+      </Text>
+    </Container>
     <HardwareGrants />
     <Box
        as="footer"
