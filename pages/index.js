61086db1a951f10aad600718de7534ecd6a9b77c
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-07-31 20:14:30 +0800

    Add Hardware Intro (written by Chris)

--- pages/index.js
+++ pages/index.js
@@ -71,47 +71,44 @@ export default () => (
           fontSize: '24px'
         }}
       >
-        This will be changed to something :) 53 years ago, 100,000 people gathered in San
-        Francisco for the Summer of Love. Welcome to the Summer of Making. It’s
-        not an event or program. It’s a theme for the summer, a challenge to
-        every teenager reading this: What will you make? To suppport you, we’re
-        distributing $50,000 in free electronics (thanks GitHub!) & building
-        Snapchat streaks for coding—but really Summer of Making is about you. If
-        this will be the best summer ever, it’ll be because of what you choose
-        to hack on & learn from.
+        We focus a lot on software at Hack Club. It's soft, after all.
+        Comfortable. This summer we left our comfort zone. Global
+        pandemic—generally a bad start to summer—left us with $50k of unused
+        grant money from GitHub. We thought about saving it. Instead we
+        gave it away to students as a kind of fun experiment. One thing we liked about
+        hardware grants was that bits—however magical—are ultimately constrained
+        by atoms. It seemed right to connect over building physical things during
+        a summer when personal connections relied on the internet more than ever.
       </Text>
     </Container>
     <HardwareGrants />
-    <Box
-       as="footer"
-       sx={{py: [4, 5], a: { color: 'orange' } }}
-     >
-       <Container>
-         <Heading as="h3" variant="subheadline" mb={2}>
-           A project by <a href="https://hackclub.com/">Hack Club</a>.
-         </Heading>
-         <Text as="p" variant="caption" mb={3}>
-           Thank you: Sam Poder, Lachlan Campbell, Zach Latta,
-           Roshan Palakkal, Neel Redkar, Matthew Stanciu, Chris Walker, Max Wofford.
-         </Text>
-         <Text as="p" variant="caption" mb={1}>
-           Site by <a href="https://lachlanjc.com/">@lachlanjc</a>. Always{' '}
-           <a href="https://github.com/hackclub/summer/">open source</a>. Graphics
-           from <a href="https://github.com/fritzing/fritzing-parts">Fritzing</a>
-           .
-         </Text>
-         <Text as="p" variant="caption">
-           Following the{' '}
-           <a href="https://hackclub.com/conduct/">Hack Club Code of Conduct</a>.
-         </Text>
-         <Text
-           as="p"
-           variant="caption"
-           sx={{ mt: 3, pt: 3, borderTop: '1px solid', borderColor: '#C0CCDA' }}
-         >
-           © 2020 Hack Club. 501(c)(3) nonprofit (EIN: 81-2908499)
-         </Text>
-       </Container>
-     </Box>
+    <Box as="footer" sx={{ py: [4, 5], a: { color: 'orange' } }}>
+      <Container>
+        <Heading as="h3" variant="subheadline" mb={2}>
+          A project by <a href="https://hackclub.com/">Hack Club</a>.
+        </Heading>
+        <Text as="p" variant="caption" mb={3}>
+          Thank you: Sam Poder, Lachlan Campbell, Zach Latta, Roshan Palakkal,
+          Neel Redkar, Matthew Stanciu, Chris Walker, Max Wofford.
+        </Text>
+        <Text as="p" variant="caption" mb={1}>
+          Site by <a href="https://lachlanjc.com/">@lachlanjc</a>. Always{' '}
+          <a href="https://github.com/hackclub/summer/">open source</a>.
+          Graphics from{' '}
+          <a href="https://github.com/fritzing/fritzing-parts">Fritzing</a>.
+        </Text>
+        <Text as="p" variant="caption">
+          Following the{' '}
+          <a href="https://hackclub.com/conduct/">Hack Club Code of Conduct</a>.
+        </Text>
+        <Text
+          as="p"
+          variant="caption"
+          sx={{ mt: 3, pt: 3, borderTop: '1px solid', borderColor: '#C0CCDA' }}
+        >
+          © 2020 Hack Club. 501(c)(3) nonprofit (EIN: 81-2908499)
+        </Text>
+      </Container>
+    </Box>
   </Box>
 )
