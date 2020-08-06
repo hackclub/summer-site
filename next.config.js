f6506a680107301060ac6c0f1f9dcd07fca38d5d
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-08-06 19:54:46 +0800

    Add Arduino Info Page (first draft)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   assetPrefix: process.env.NODE_ENV === 'production' ? '/summer' : ''
 }
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+})
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'mdx'],
+})
