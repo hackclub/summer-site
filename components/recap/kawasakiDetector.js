3aa125637d7ba1cad1db84027d7765bc6ca56c99
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-08-23 16:39:10 +0800

    cool back grounds

--- components/recap/kawasakiDetector.js
+++ components/recap/kawasakiDetector.js
@@ -29,6 +29,7 @@ export default () => (
       overflow: 'hidden',
       height: '75%',
       background: '#ff8c37',
+      backgroundImage: "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cg fill='%23ea7f30' fill-opacity='0.4'%3E%3Cpolygon fill-rule='evenodd' points='8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4'/%3E%3C/g%3E%3C/svg%3E\")",
       clear: 'both'
     }}
   >
--- components/recap/midi.js
+++ components/recap/midi.js
@@ -28,6 +28,7 @@ export default () => (
       overflow: 'hidden',
       height: '75%',
       background: '#33d6a6',
+      backgroundImage: "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%231bb880' fill-opacity='0.4'%3E%3Cpath d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E\")",
       clear: 'both'
     }}
   >
@@ -83,7 +84,7 @@ export default () => (
         >
           <Heading>Using hardware to make</Heading>
           <Heading as="h1" sx={{ fontSize: [6, 7, 7] }}>
-            epic beats
+            fire beats
           </Heading>
           <Text
             sx={{
