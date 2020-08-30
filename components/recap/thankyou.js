0b9aa7b71edd30fb346f3120f6f1e58ea66c7a75
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-08-30 16:16:57 +0800

    good bye!

--- components/recap/thankyou.js
+++ components/recap/thankyou.js
@@ -44,7 +44,8 @@ const SheetOLd = () => (
       minHeight: '500px',
       backdropFilter: 'blur(20px)',
       backgroundPosition: 'center center',
-      animation: 'thankyoubg 5s infinite',
+      backgroundImage:
+        'radial-gradient(at left top,rgba(247,255,0,0.66),rgb(0,255,255,0.66)), url("https://cloud-qmfmc6mn0.vercel.app/ezgif-4-727ac1653cd9-2.gif")'
     }}
   >
     <Container
@@ -241,4 +242,4 @@ const MAINPART = () => {
   }
 }
 
-export default SheetOLd
+export default MAINPART
