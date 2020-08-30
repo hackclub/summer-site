ccdb3fa2d69b84ccf84598840a56784468dd9bc3
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-08-30 21:51:00 +0800

    Remove whit space!

--- components/recap/AMA.js
+++ components/recap/AMA.js
@@ -37,27 +37,27 @@ export default () => (
         mt={[3, 4]}
         sx={{ alignItems: 'center', justifyContent: 'center' }}
       >
-        <Card style={{position: 'relative', background: 'none'}} as="a" href="https://www.youtube.com/watch?v=IWFtj9cCaB0" variant="interactive">
+        <Card style={{position: 'relative', height: '200px', background: 'none'}} as="a" href="https://www.youtube.com/watch?v=IWFtj9cCaB0" variant="interactive">
           <Image src="https://cloud-8be4hh8an.vercel.app/limor-fried-600x600-2-2.jpg" style={{ borderRadius: '10px' }} />
-          <p style={{position: 'absolute', bottom: '0.4em', width:'100%', textAlign: 'center', background: 'rgba(0, 0, 0, 0.3)', color: 'white', marginBlockEnd: '0em', borderRadius: '10px'}}>Limor Fried</p>
+          <p style={{position: 'absolute', bottom: '0.4em', width:'100%', textAlign: 'center', background: 'rgba(0, 0, 0, 0.3)', color: 'white', marginBlockEnd: '0em' }}>Limor Fried</p>
         </Card>
-        <Card style={{position: 'relative', background: 'none'}} as="a" href="https://www.youtube.com/watch?v=fDKYjX37cbo" variant="interactive">
+        <Card style={{position: 'relative', height: '200px', background: 'none'}} as="a" href="https://www.youtube.com/watch?v=fDKYjX37cbo" variant="interactive">
           <Image src="https://cloud-b3z9crw81.vercel.app/4946a9bca0943aa959cb6c8b72c0cc17-2.png" style={{ borderRadius: '10px' }} />
-          <p style={{position: 'absolute', bottom: '0.4em', width:'100%', textAlign: 'center', background: 'rgba(0, 0, 0, 0.3)', color: 'white', marginBlockEnd: '0em', borderRadius: '10px'}}>Dylan Field</p>
+          <p style={{position: 'absolute', bottom: '0.4em', width:'100%', textAlign: 'center', background: 'rgba(0, 0, 0, 0.3)', color: 'white', marginBlockEnd: '0em' }}>Dylan Field</p>
         </Card>
-        <Card style={{position: 'relative', background: 'none'}} as="a" href="https://www.youtube.com/watch?v=tDtBCcLJ2xU" variant="interactive">
+        <Card style={{position: 'relative', height: '200px', background: 'none'}} as="a" href="https://www.youtube.com/watch?v=tDtBCcLJ2xU" variant="interactive">
           <Image src="https://cloud-lrygi0a7b.vercel.app/tommy-devoss_trans__nwjyn5afh1f8m-ugcq32yesxdvobhlbzzp-a_kknc4m-2.jpg" style={{ borderRadius: '10px' }} />
-          <p style={{position: 'absolute', bottom: '0.4em', width:'100%', textAlign: 'center', background: 'rgba(0, 0, 0, 0.3)', color: 'white', marginBlockEnd: '0em', borderRadius: '10px'}}>Tommy DeVoss</p>
+          <p style={{position: 'absolute', bottom: '0.4em', width:'100%', textAlign: 'center', background: 'rgba(0, 0, 0, 0.3)', color: 'white', marginBlockEnd: '0em' }}>Tommy DeVoss</p>
         </Card>
-        <Card style={{position: 'relative', background: 'none'}} as="a" href="https://www.youtube.com/watch?v=thXsjHVcxx4" variant="interactive">
+        <Card style={{position: 'relative', height: '200px', background: 'none'}} as="a" href="https://www.youtube.com/watch?v=thXsjHVcxx4" variant="interactive">
           <Image src="https://dl.airtable.com/.attachmentThumbnails/df51be717dd8e09204ec23b27336f4a7/acff2db0" style={{ borderRadius: '10px' }} />
-          <p style={{position: 'absolute', bottom: '0.4em', width:'100%', textAlign: 'center', background: 'rgba(0, 0, 0, 0.6)', color: 'white', marginBlockEnd: '0em', borderRadius: '10px'}}>Alex Stamos</p>
+          <p style={{position: 'absolute', bottom: '0.4em', width:'100%', textAlign: 'center', background: 'rgba(0, 0, 0, 0.6)', color: 'white', marginBlockEnd: '0em' }}>Alex Stamos</p>
         </Card>
-        <Card style={{position: 'relative', background: 'none'}} as="a" href="https://www.youtube.com/watch?v=KKEYTSUvsS8" variant="interactive">
+        <Card style={{position: 'relative', height: '200px', background: 'none'}} as="a" href="https://www.youtube.com/watch?v=KKEYTSUvsS8" variant="interactive">
           <Image src="https://cloud-n74ym17dx.vercel.app/540px-nicky_case_-_game_developers_conference_2019_-_03.jpg" style={{ borderRadius: '10px' }} />
-          <p style={{position: 'absolute', bottom: '0.4em', width:'100%', textAlign: 'center', background: 'rgba(0, 0, 0, 0.3)', color: 'white', marginBlockEnd: '0em', borderRadius: '10px'}}>Nicky Case</p>
+          <p style={{position: 'absolute', bottom: '0.4em', width:'100%', textAlign: 'center', background: 'rgba(0, 0, 0, 0.3)', color: 'white', marginBlockEnd: '0em' }}>Nicky Case</p>
         </Card>
       </Grid>
-      <Button mt={3,4} as="a" href="https://www.youtube.com/channel/UCQzO0jpcRkP-9eWKMpJyB0w">Watch more on Youtube</Button>
+      <Button mt={4,5} as="a" href="https://www.youtube.com/channel/UCQzO0jpcRkP-9eWKMpJyB0w">Watch more on Youtube</Button>
     </Box>
 )
