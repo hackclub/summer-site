69a336d6540a6878a46a5cc697cf5e75c526447a
Lachlan Campbell <lachlan@hackclub.com>
2020-08-31 23:49:45 +0000

    Homepage fixes

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ import Thanks from '../components/recap/thankyou'
 import Intro from '../components/recap/intro'
 import HardwareIntro from '../components/recap/hardware-intro'
 import ScrapbookIntro from '../components/recap/scrapbook-intro'
-import Footer from '../components/footer'
+import Footer from '../components/recap/footer'
 
 export default () => (
   <>
