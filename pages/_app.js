5ee274ccecac19e7a2c63b2c73d7e15385edf3b5
Max Wofford <max@maxwofford.com>
2021-03-22 15:03:07 -0400

    Remove Fathom analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,14 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useRouter } from 'next/router'
-import NextApp from 'next/app'
 
-import Meta from '@hackclub/meta'
 import theme from '../lib/theme'
 import theme2 from '../lib/theme2'
 import { ThemeProvider } from 'theme-ui'
-import * as Fathom from 'fathom-client'
 import '../public/theme.css'
 import '@hackclub/theme/fonts/reg-bold.css'
 
 const App = ({ Component, pageProps }) => {
   const router = useRouter()
-
-  useEffect(() => {
-    // the hackclub.com code
-    Fathom.load('OGIMJEFA', {
-      includedDomains: ['summer.hackclub.com', 'hackclub.com']
-    })
-    const onRouteChangeComplete = () => Fathom.trackPageview()
-    router.events.on('routeChangeComplete', onRouteChangeComplete)
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete)
-    }
-  }, [])
 
   return (
     <ThemeProvider theme={router.pathname === '/' ? theme2 : theme}>
