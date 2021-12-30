b173ee6f1b0bf8e7c9f503fcec0478b4e65c7119
Ella <72365100+eilla1@users.noreply.github.com>
2021-12-29 20:57:00 -0800

    Fix scrapbook widget cors error (#134)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
+
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'mdx'],
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/summer' : ''
+  extension: /\.mdx?$/,
+  options: {
+    pageExtensions: ['js', 'jsx', 'mdx'],
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/summer' : ''
+  },
+  async headers() {
+    return [
+      {
+        source: '/scrapbookwidget.js',
+        headers: [{ key: 'Access-Control-Allow-Origin', value: '*' }]
+      }
+    ]
+  }
 })
